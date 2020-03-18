38d5ded2bdc3077fab8a64d4d121f65f85bbad89
trestletech <github@trestletech.com>
2020-03-18 15:20:25 -0500

    First pass on manifests

--- utils/updateManifests.R
+++ utils/updateManifests.R
@@ -1,10 +1,38 @@
 # Make sure our library is current
 renv::restore()
 
-files <- list.files()
-files <- files[files != "renv"]
+# The currrent list as per Garrett
+officialPrimers <-
+  c("./basics/02-Vis-Basics/02-Vis-Basics.Rmd","./basics/02-Vis-Basics/02-Vis-Basics.html",
+    "./basics/04-Programming-Basics/04-Programming-Basics.Rmd",
+    "./iterate/list-columns/list-columns.Rmd",
+    "./iterate/map-shortcuts/map-shortcuts.Rmd",
+    "./iterate/multiple-vectors/multiple-vectors.Rmd",
+    "./iterate/introduction-to-iteration/introduction-to-iteration.Rmd",
+    "./iterate/map/map.Rmd",
+    "./write-functions/advanced-control-flow/advanced-control-flow.Rmd",
+    "./write-functions/environments/environments.Rmd",
+    "./write-functions/function-basics/function-basics.Rmd",
+    "./write-functions/how-to-write-a-function/how-to-write-a-function.Rmd",
+    "./write-functions/control-flow/control-flow.Rmd",
+    "./write-functions/loops/loops.Rmd",
+    "./write-functions/arguments/arguments.Rmd",
+    "./transform-data/03-deriving/03-deriving.Rmd",
+    "./transform-data/02-isolating/02-isolating.Rmd",
+    "./transform-data/01-tibbles/01-tibbles.Rmd",
+    "./visualize-data/08-Customize/08-Customize.Rmd",
+    "./visualize-data/01-Exploratory-Data-Analysis/01-Exploratory-Data-Analysis.Rmd",
+    "./visualize-data/01-bar-charts/01-bar-charts.Rmd",
+    "./visualize-data/07-overplotting/07-overplotting.Rmd",
+    "./visualize-data/03-histograms/03-histograms.Rmd",
+    "./visualize-data/05-scatterplots/05-scatterplots.Rmd",
+    "./visualize-data/04-boxplots/04-boxplots.Rmd",
+    "./visualize-data/06-line-graphs/06-line-graphs.Rmd",
+    "./tidy-data/03-join-datasets/03-join-datasets.Rmd",
+    "./tidy-data/01-Reshape-Data/01-Reshape-Data.Rmd",
+    "./tidy-data/02-separate-columns/02-separate-columns.Rmd")
 
-allRMDs <- list.files(files, "*.Rmd", recursive=TRUE, ignore.case=TRUE, full.names=TRUE)
+allRMDs <- officialPrimers
 allDirs <- unique(dirname(allRMDs))
 
 wd <- getwd()
